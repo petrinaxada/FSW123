9fdf18fb6d5d719c041f643dd07994c512323fd6
Petrina Ada <petrina.ada@bryanuniversity.edu>
2022-09-14 21:47:42 -0700

    my-app

---
+++ Week1/my-app/src/ToDoList.js
@@ -0,0 +1,12 @@
+function ToDoList(){
+    return (
+     <div>
+        <h1>To Do ToDoList</h1>
+       <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+</ul></div>
+    );
+    }
+export default ToDoList; 
